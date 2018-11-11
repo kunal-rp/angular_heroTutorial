9bbf9b28ac87af2278991f050d1cf23c927cc74a
KRP <kunal.r.rajpurohit@gmail.com>
2018-11-11 00:22:58 -0800

    Adding two way binding to heros component 	imported directive from the 'FormsModule' 	adding in inpit field to the heros component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HerosComponent } from './heros/heros.component';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +12,8 @@ import { HerosComponent } from './heros/heros.component';
     HerosComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
